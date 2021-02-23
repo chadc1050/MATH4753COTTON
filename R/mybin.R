aa66b330b90d969cce4c2ba899bce0153f08d79b
Chad Cotton <chad.cotton.2000@ou.edu>
2021-02-22 20:06:02 -0600

    Fixed mybin() function implementation

--- R/mybin.R
+++ R/mybin.R
@@ -7,17 +7,13 @@
 #' @return Number of successes in each simulation
 #' @export
 #'
-#' @examples
-#' iter <- 100, n <- 10, p <- 0.5
-mybin=function(iter,n, p){
-  # make a matrix to hold the samples
-  #initially filled with NA's
-  sam.mat=matrix(NA,nr=n,nc=iter, byrow=TRUE)
-  #Make a vector to hold the number of successes in each trial
-  succ=c()
+#'
+
+mybin <- function(iter, n, p) {
+  sam.mat = matrix(NA, nr = n, nc = iter, byrow = TRUE)
+  succ = c()
   for( i in 1:iter){
-    #Fill each column with a new sample
-    sam.mat[,i]=sample(c(1,0),n,replace=TRUE, prob=c(p,1-p))
-    #Calculate a statistic from the sample (this case it is the sum)
-    succ[i]=sum(sam.mat[,i])
+    sam.mat[,i] = sample(c(1,0), n, replace = TRUE, prob = c(p, 1 - p))
+    succ[i] = sum(sam.mat[,i])
   }
+}
